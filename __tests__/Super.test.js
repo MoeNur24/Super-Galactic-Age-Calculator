e103096c7bb347a7375cb491ef824c7a85f2a777
Mohamed Nur <mohamednur@Mohameds-MacBook-Pro-2.local>
2023-12-24 12:00:28 -0800

    See if test verify constructor is working correctly

--- __tests__/Super.test.js
+++ __tests__/Super.test.js
@@ -2,6 +2,13 @@ describe('GalacticAge', () => {
     let galacticAge;
 
     beforeEach(() => {
-        galacticAge = new galacticAge(25);
+        galacticAge = new galacticAge(23);
     });
+
+    test('constructor sets age properly', () => {
+        const age = 35
+        const galacticAge = new GalacticAge(age);
+        expect(galacticAge.onEarth()).toBe(age);
+    });
+    
 });

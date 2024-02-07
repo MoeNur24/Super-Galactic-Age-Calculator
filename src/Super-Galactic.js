8cb93d646a6ee781d7bdf1512c45e2858053ea7e
Mohamed Nur <mohamednur@Mohameds-MacBook-Pro-2.local>
2024-02-06 16:55:42 -0800

    JS

--- src/Super-Galactic.js
+++ src/Super-Galactic.js
@@ -22,4 +22,22 @@ export class SpaceAge{
   jupiterAge() {
     return parseFloat((this.age / 11.86).toFixed(2));
   }
+yearsPassed(pastBirthday) {
+  const earthYears = this.age - pastBirthday;
+  return {earthYears,
+    mercuryYears: parseFloat((earthYears / .24).toFixed(2)),
+    venusYears: parseFloat((earthYears / .62).toFixed(2)),
+    marsYears: parseFloat((earthYears / 1.88).toFixed(2)),
+    jupiterYears: parseFloat((earthYears / 11.86).toFixed(2)),
+  };
+}
+yearsLeft(futureBirthday) {
+  const earthLeft = futureBirthday - this.age;
+  return {earthYears: earthLeft,
+    mercuryYears: parseFloat((earthLeft / .24).toFixed(2)),
+    venusYears: parseFloat((earthLeft / .62)),
+    marsYears: parseFloat((earthLeft / 1.88).toFixed(2)),
+    jupiterYears: parseFloat((earthLeft / 11.86).toFixed(2)),
+  };
+}
 }

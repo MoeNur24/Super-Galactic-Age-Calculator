0581ee871ec372fec0ac644b0ac5e269a8e6fe31
Mohamed Nur <mohamednur@Mohameds-MacBook-Pro-2.local>
2024-02-08 13:44:31 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import {} from './Person.js';
+import './css/styles.css';
+import { SpaceAge } from './Super-Galactic.js';
+
+
+document.getElementById('space-age-form').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const age = parseInt(document.getElementById('age').value, 10);
+  const pastBirthday = parseInt(document.getElementById('past-birthday').value, 10);
+  const futureBirthday = parseInt(document.getElementById('future-birthday').value, 10);
+
+  const spaceAge = new SpaceAge(age);
